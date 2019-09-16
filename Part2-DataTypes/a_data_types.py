d80b400b7efc4e67e93a9cd76ca900f0619d3cc4
coroush <soroush.garivani@iaac.net>
2019-09-16 16:21:41 +0200

    add header  Please enter the commit message for your changes. Lines starting

--- Part2-DataTypes/a_data_types.py
+++ Part2-DataTypes/a_data_types.py
@@ -1,3 +1,26 @@
+# encoding: utf-8
+
+##################################################
+# This script shows an example of a header section. This sections is a group of commented lines (lines starting with
+# the "#" character) that describes general features of the script such as the type of licence (defined by the
+# developer), authors, credits, versions among others
+##################################################
+#
+##################################################
+# Author: Soroush Garivani
+# Copyright: Copyright 2019, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.0.0
+# Maintainer: Soroush Garivani
+# Email: soroush.garivani@iaac.net
+# Status: development
+##################################################
+
+# End of header section
+
+
+
 # declaring a value for a variable, in this case an Integer
 print ("my_data is")
 my_data = 5
