2dbe24f91561880abdd2c6558dbdefde84f2e89e
diego <diegopajarito@gmail.com>
2019-09-20 17:57:07 +0200

    Updating scripts for part 3 and data sets

--- Part3-ExternalSources/b_return_values.py
+++ Part3-ExternalSources/b_return_values.py
@@ -47,11 +47,40 @@ def get_averages(values1, values2):
 cities = {'names': ['Barcelona', 'Lisbon', 'Amsterdam'],
           'population': [5474482, 2827514, 2431000],
           'unemployment_rate': [17.24, 7.4, 3.3],
-          'gdb_billions': [173, 72, 154]}
+          'gdp_billions': [173, 72, 154]}
 text_template = 'Average population is: %f \n Average unemployment rate is: %f'
 
-val1, val2 = get_averages(cities['population'], cities['unemployment_rate'])
+val1, val2 = get_averages(cities['population'], cities['gdp_billions'])
 
-print(text_template % (val1, val2))
+#print(text_template % (val1, val2))
 
-get_proportions(cities['gdb_billions'], cities['names'], 'GDP')
+# This function prints the city names and GDP values using a template. Check it out above
+get_proportions(cities['gdp_billions'], cities['names'], 'GDP')
+
+
+# Now we proceed to calculate the GDP per capita. First, we create an empty list and lists for population and GDP
+per_capita = []
+gdp_billions =  cities['gdp_billions']
+population = cities['population']
+# Ths for loop goes across cities
+for i in range(len(cities['gdp_billions'])):
+    # Here we transform GDP units from billions to millions. 1b = 1000m
+    gdp_mill = cities['gdp_billions'][i] * 1000
+    # Here we transform population to millions. 1m = pop / 1000000
+    pop_mill = population[i] / 1000000
+    gdp_percapita = gdp_mill / pop_mill
+    per_capita.append(gdp_percapita)
+
+# We print the list in which we appended all individual values
+print(per_capita)
+
+# See this formula as an exmaple of GDP per capita estimation
+#cities['gdp_percapita'] = ( cities['gdp_billions'] * 1000 ) / ( cities['population'] / 1000000)
+
+
+#######################
+# Side note, calling lists within lists. It works also for dictionaries.
+my_list = [["a","b","c"],["d","e","f"]]
+
+# print first item of second list in my_list
+print (my_list[1][0])
